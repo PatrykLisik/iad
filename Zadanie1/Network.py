f4e05506dcb4e6cf432cce72657db9849e7188a0
SzymonSzymajda <szymonicho@gmail.com>
2018-03-09 17:30:15 +0100

    another bias fix

--- Zadanie1/Network.py
+++ Zadanie1/Network.py
@@ -23,27 +23,31 @@ class NeuralNetwork:
 
     def train(self, inputs, targets):
         hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_inputs += self.bih
         hidden_outputs = self.activation_function(hidden_inputs)
         final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_inputs += self.bho
         final_outputs = self.activation_function(final_inputs)
         output_errors = targets - final_outputs
         hidden_errors = numpy.dot(self.who.T, output_errors)
         self.who += self.lr * numpy.dot((output_errors * final_outputs *
                                         (1.0 - final_outputs)),
                                         numpy.transpose(hidden_outputs))
-        self.bho+=self.lr * output_errors * final_outputs * (1.0 - final_outputs)
+        self.bho += self.lr * output_errors * final_outputs * (1.0 -
+                                                               final_outputs)
         self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs *
                                         (1.0 - hidden_outputs)),
                                         numpy.transpose(inputs))
-        self.bih+=self.lr * hidden_errors * hidden_outputs * (1.0 - hidden_outputs)
+        self.bih += self.lr * hidden_errors * hidden_outputs * (1.0 -
+                                                                hidden_outputs)
         pass
 
     def query(self, inputs):
         hidden_inputs = numpy.dot(self.wih, inputs)
-        hidden_inputs+=self.bih
+        hidden_inputs += self.bih
         hidden_outputs = self.activation_function(hidden_inputs)
         final_inputs = numpy.dot(self.who, hidden_outputs)
-        final_inputs+=self.bho
+        final_inputs += self.bho
         final_outputs = self.activation_function(final_inputs)
         return final_outputs
 
