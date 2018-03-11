56dcf5ce524de8f72408d55b4993eaf2c8498e7a
PatrykLisik <patrykkbkak@gmail.com>
2018-03-11 12:43:02 +0100

    import from csv, tweeks in plot

--- Rozgrzewka_2/program.py
+++ Rozgrzewka_2/program.py
@@ -1,40 +1,68 @@
+#!/usr/bin/env python3
 import numpy as np
 from matplotlib.pyplot import cm
 import matplotlib.pyplot as plt
 from Aprox import *
+import csv
+import sys
 
+#Mean squared error
+def MSE(f,intput,output):
+    ans=0;
+    for i in range(len(output)):
+        ans+=((f(intput[i])-output[i])**2)
+    return ans/len(output)
 
+def getData(intput):
+    reader = csv.reader(intput)
+    outX=[]
+    outY=[]
+    for row in reader:
+        i = list(map(float,row[0].split(";")))
+        outX.append(i[0:-1][0])
+        outY.append(i[-1])
+    return outX,outY
+
+intput_file = open(sys.argv[1], "r+")
+x,y=getData(intput_file)
+intput_file.close()
+print("X: ",x,"\nY:",y)
 #points
-PointsX=[-0.2, 0.4, 0.6, 1.2, 1.9, 0.5]
-PointsY=[0, 0, 1, 1, 1, 0]
+PointsX=x
+PointsY=y
 
 #Set up plot
 plt.figure(figsize=(10,10))
-plt.xlim([-3,3])
-plt.ylim([-0.5,1.5])
 plt.grid()
 plt.title("")
-plt.scatter(PointsX,PointsY,color='black', s=10)
-
-#Arry with number of iteration when aproximation is plotted
-iterToPlot=[0,10,20,100,10000,100000]
-#Colors of plots
-colors=iter(cm.rainbow(np.linspace(0,1,len(iterToPlot))))
+#plt.ylim([0,0.02])
+#plt.xscale("log")
+plt.yscale("log")
+plt.xlabel("Number of iterations",fontsize="xx-large")
+plt.ylabel("Mean squared error", fontsize="xx-large")
 
 aprox=Aprox(1)
-x = np.arange(-3, 3, 0.01);
 
-itr=500000
-for k in range(itr):
+Error=1
+ErrorY=[]
+ErrorX=[]
+nOfIter=0
+while Error>10**(-4):
+    Error=MSE(aprox,PointsX,PointsY);
     for i in range(len(PointsX)):
         aprox.updateWeigths(PointsX[i],PointsY[i])
-    if k in iterToPlot:
-        y1=aprox.map(x)
-        c=next(colors)
-        plt.plot(x,y1,color=c, label="After {0} iterations".format(k))
+    ErrorY.append(Error)
+    ErrorX.append(nOfIter)
+    if nOfIter%20000==0:
+        print("Error:",Error)
+        print("Iteration:",nOfIter)
+    nOfIter+=1;
+#print to stdout
+for i in aprox.W:
+    print(i[0])
+print(aprox.w0[0][0])
 
-y1=aprox.map(x)
-plt.plot(x,y1,color='purple',label="final")
-legend = plt.legend(loc='best',
-          ncol=3, fancybox=True, shadow=True)
-plt.savefig("wykres.png")
+print("total number of iterations:",nOfIter)
+
+plt.plot(ErrorX,ErrorY,color='purple')
+plt.savefig("Errors1.png")
