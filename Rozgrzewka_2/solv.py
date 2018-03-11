0e1562d5926d722de294214b703909ca8948b5b5
PatrykLisik <patrykkbkak@gmail.com>
2018-03-11 08:00:49 +0100

    OOP approach, bigger plot

--- Rozgrzewka_2/solv.py
+++ Rozgrzewka_2/solv.py
@@ -1,45 +1,68 @@
 import numpy as np
+from matplotlib.pyplot import cm
 import matplotlib.pyplot as plt
-from matplotlib import rc
+
+class Aprox(object):
+    def __init__(self, dimm_number):
+        self.W=np.random.rand(1,dimm_number)
+        self.w0=np.random.rand()
+        #step aka learnig rate
+        self.step=0.1
+    def sigmoid(self,X):
+        #Muliti-dimensional sigmoid
+        #output is matrix is 1x1,
+        return 1/(1+np.exp(np.inner(X,self.W)+self.w0))
+    def grad(self,X,y):
+        #Notice that it is not multiplay by x_i
+        f=self.sigmoid
+        return (f(X)-y)*f(X)*(1-f(X))
+    def __call__(self,X):
+        #output of sigmoid is matrix is 1x1, so one value is returned
+        return self.sigmoid(X)[0][0]
+    def updateWeigths(self,X,y):
+        #basicly learnig
+        g=self.grad(X,y)*self.step
+        self.W+=g*X;
+        self.w0+=g
+    def map(self,x):
+        ans=[]
+        for i in x:
+            ans.append(self.__call__(i))
+        return ans
+
+
 
 #points
 PointsX=[-0.2, 0.4, 0.6, 1.2, 1.9, 0.5]
 PointsY=[0, 0, 1, 1, 1, 0]
-dimm_number=1 #number of dimmensions
-W=np.random.rand(1,dimm_number)
-X=np.random.rand(1,dimm_number)
-w0=np.random.rand()
-f=lambda X:1/(1+np.exp(np.inner(X,W)+w0))
-grad=lambda X,y:(f(X)-y)*f(X)*(1-f(X))
-print("W:",W,"\nX:",X,"\ngrad",grad(-0.2,0))
 
-x = np.arange(-3, 3, 0.01);
-y1=[]
-for i in x:
-    y1.append(f(i)[0][0])
-
-plt.plot(x,y1,color='red')
-
-itr=2000000
-for k in range(itr):
-    for i in range(len(PointsX)):
-        W+=(grad(PointsX[i],PointsY[i])*PointsX[i])
-        w0+=grad(PointsX[i],PointsY[i])
-    if (k %(itr/20))==0 or k==[1,10,100]:
-        print(W,w0)
-        y1=[]
-        for j in x:
-            y1.append(f(j)[0][0])
-        plt.plot(x,y1,color='blue')
-print(W,w0)
-y1=[]
-for j in x:
-    y1.append(f(j)[0][0])
-plt.plot(x,y1,color='green')
+plt.figure(figsize=(10,10))
 #range
 plt.xlim([-3,3])
 plt.ylim([-0.5,1.5])
 plt.grid()
 plt.title("Rozgrzewka 2")
 plt.scatter(PointsX,PointsY,color='black', s=10)
+
+#Arry with number of iteration when aproximation is plotted
+iterToPlot=[0,10,20,100,10000,100000]
+#Colors of plots
+colors=iter(cm.rainbow(np.linspace(0,1,len(iterToPlot))))
+
+aprox=Aprox(1)
+x = np.arange(-3, 3, 0.01);
+
+itr=500000
+for k in range(itr):
+    for i in range(len(PointsX)):
+        aprox.updateWeigths(PointsX[i],PointsY[i])
+    if k in iterToPlot:
+        y1=aprox.map(x)
+        c=next(colors)
+        plt.plot(x,y1,color=c, label="After {0} iterations".format(k))
+
+y1=aprox.map(x)
+plt.plot(x,y1,color='pink',label="final")
+legend = plt.legend(loc='best',
+          ncol=3, fancybox=True, shadow=True)
 plt.savefig("wykres.png")
