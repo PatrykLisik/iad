78af6e77bfebd128aa733a08cb3159b098b4b733
Patryk Lisik <patrykkbkak@gmail.com>
2018-03-31 09:10:46 +0200

    output layer activation function and its derivative can be changed

--- Zadanie1/NeutralNetwork.py
+++ Zadanie1/NeutralNetwork.py
@@ -3,7 +3,9 @@ import scipy.special
 
 
 class NeutralNetwork:
-    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate=0.1, bias_mult=0,monemtum_rate=0):
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate=0.1, bias_mult=0,
+                        monemtum_rate=0,activation_function_output=lambda x: scipy.special.expit(x),
+                        dactivation_function_output=lambda x: scipy.special.expit(x)*(1-scipy.special.expit(x))):
         self.inodes = input_nodes
         self.hnodes = hidden_nodes
         self.onodes = output_nodes
@@ -16,6 +18,8 @@ class NeutralNetwork:
         self.momentum=monemtum_rate;
         self.beta=0.1;
         self.activation_function = lambda x: scipy.special.expit(x)
+        self.activation_function_output=activation_function_output
+        self.dactivation_function_output=dactivation_function_output
         pass
     def train(self, input_list, target_list):
         inputs=numpy.array(input_list, ndmin=2).T
@@ -25,7 +29,7 @@ class NeutralNetwork:
         hidden_outputs = self.activation_function(hidden_inputs)
         final_inputs = numpy.dot(self.who, hidden_outputs)
         final_inputs += self.bho
-        final_outputs = self.activation_function(final_inputs)
+        final_outputs = self.activation_function_output(final_inputs)
 
         output_errors = targets - final_outputs
         hidden_errors = numpy.dot(self.who.T, output_errors)
@@ -39,11 +43,11 @@ class NeutralNetwork:
                                             final_outputs)*self.bias_mult
 
         wih_back=self.wih # momentum
-        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs *
-                                        (1.0 - hidden_outputs)),numpy.transpose(inputs))
+        self.wih += self.lr * numpy.dot((hidden_errors * self.dactivation_function_output(hidden_outputs)),
+                                        numpy.transpose(inputs))
         self.wih+=self.wih*self.momentum
 
-        self.bih += self.lr * hidden_errors * hidden_outputs * (1.0 - hidden_outputs)
+        self.bih += self.lr * hidden_errors * self.dactivation_function_output(hidden_outputs)
         self.bih*=self.bias_mult
 
     def query(self, input_list):
