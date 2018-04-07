3340b05524e5eb4a279fc4df6d7f0baee7196f45
Patryk Lisik <patrykkbkak@gmail.com>
2018-04-07 17:26:19 +0200

    working gif generation

--- Rozgrzewka_4/program.py
+++ Rozgrzewka_4/program.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.animation as animation
 
 def getPointsInCircle(center,radius,amount):
     centerX=center[0]
@@ -41,25 +42,36 @@ def plotChart(x,y,r_x,r_y,out):
         ax.add_artist(c2)
         plt.savefig(out)
 
-def plotPointsOfDict(x,y,r_x,r_y,data,out):
-        #colors=iter(cm.Set1(np.linspace(0,1,len(data))))
-        #Set up plot
-        fig=plt.figure(figsize=(10,10))
-        ax = fig.add_subplot(111)
-        plt.grid()
-        ax.scatter(x,y,color='purple')
-        ax.scatter(r_x,r_y,color='red')
-        c1=plt.Circle([-3,0],2,alpha=0.5)
-        c2=plt.Circle([3,0],2,alpha=0.5)
-        for red,blues in data.items():
-            for b in blues:
-                plt.plot([red[0],b[0]],[red[1],b[1]],'--', lw=0.3,color="black")
+def animate(num,data,line):
+    #line.set_data(np.arange(0,num,0.01),np.sin(np.arange(0,num,0.01)))
+    #plt.cla()
+    plt.xlabel("Iteracja-{0}".format(num))
+    for red,blues in data[num].items():
+        plt.scatter(red[0],red[1],color="red")
+        for b in blues:
+            pass
+            #plt.plot([red[0],b[0]],[red[1],b[1]],'--', lw=0.3,color="black")
+    return (line,)
 
-        plt.xlim([-10,10])
-        plt.ylim([-10,10])
-        ax.add_artist(c1)
-        ax.add_artist(c2)
-        plt.savefig(out)
+def plotPointsOfDict(x,y,r_x,r_y,data,out):
+    #colors=iter(cm.Set1(np.linspace(0,1,len(data))))
+    #Set up plot
+    fig=plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111)
+    anim=fig.add_subplot(111)
+    l, = plt.plot([], [], 'r-')
+    plt.grid()
+    k_mean = animation.FuncAnimation(fig, animate, len(data), fargs=(data,l),
+    interval=1000, blit=True)
+    ax.scatter(x,y,color='purple')
+    ax.scatter(r_x,r_y,color='red')
+    c1=plt.Circle([-3,0],2,alpha=0.5)
+    c2=plt.Circle([3,0],2,alpha=0.5)
+    plt.xlim([-10,10])
+    plt.ylim([-10,10])
+    ax.add_artist(c1)
+    ax.add_artist(c2)
+    k_mean.save(out+".gif", writer='imagemagick')
 
 def dist(x1,y1,x2,y2):
     return np.sqrt((x1-x2)**2+(y1-y2)**2)
@@ -72,7 +84,8 @@ y+=y1
 
 red_X,red_Y=getPointsInSquare([0,0],10,4)
 
-for n in range(3):
+list_of_red_black=[]
+for n in range(20):
     red_black={}
     for i in range(len(red_X)):
         red_black[(red_X[i],red_Y[i])]=[]
@@ -86,9 +99,7 @@ for n in range(3):
             dist_dict[d]=[red_X[red],red_Y[red]]
         a=min(dist_dict)
         red_black[(dist_dict[a][0],dist_dict[a][1])].append([x[i],y[i]])
-
-    plotPointsOfDict(x,y,red_X,red_Y,red_black,"k-średnie-iter{0}".format(n))
-
+    list_of_red_black.append(red_black)
     #new red points
     new_red_x=[]
     new_red_y=[]
@@ -101,5 +112,9 @@ for n in range(3):
         if (len(x_n)>0 and len(y_n)>0):
             new_red_x.append(np.mean(x_n))
             new_red_y.append(np.mean(y_n))
+    if(red_X==new_red_x and red_Y==new_red_y):
+        break
     red_X=new_red_x
     red_Y=new_red_y
+
+plotPointsOfDict(x,y,red_X,red_Y,list_of_red_black,"k-średnie")
