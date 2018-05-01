63096cdb35c8db734307a75d5f50763445d17e31
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-01 06:17:29 +0200

    proper 3 tab

--- Zadanie1/3/tabela.py
+++ Zadanie1/3/tabela.py
@@ -6,13 +6,14 @@ import sys
 
 import numpy as np
 
-from Functions import MSE, getAllData
-from NeutralNetwork import NeutralNetwork
-
 currentdir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
+
+from Functions import MSE, getAllData
+from NeutralNetwork import NeutralNetwork
+
 
 input_list = []
 target_list = []
@@ -25,36 +26,46 @@ input_file.close()
 results = []
 results.append(["Learning rate", "Momentum", "Ilosc iteracji", "MSE", "STD"])
 
-for k in range(100):
-    input_nodes = 4
-    hidden_nodes = 2
-    output_nodes = 4
-    learningrate = np.random.uniform(0, 0.7)
-    bias = 1  # on
-    momentum = np.random.uniform(0.1, 0.9)
-    nn = NeutralNetwork(input_nodes, hidden_nodes,
-                        output_nodes, learningrate, bias, momentum)
-    i = 0
-    error_test = 10
-    std = "???"
-    current_tuple = [learningrate, momentum]
-    while error_test > 5 * 10**-3:
-        if i > 10**5:
-            break
-        for j in range(len(input_list)):
-            nn.train(input_list[j], target_list[j])
-        f = nn.query
-        error_test = MSE(f, input_list, target_list)
-        """if i%1000==0:
-            print("ERROR: ",error_test)
-            print("i:",i)
-            print("momentum: ",momentum)"""
-        i += 1
-    current_tuple.append(i)
-    current_tuple.append(error_test)
-    current_tuple.append(std)
-    results.append(current_tuple)
-    print(current_tuple)
+learning_rates = [0.01, 0.1, 0.2, 0.4, 0.7] * 2
+momentums = [0] * 5 + list(np.linspace(0.0, 1, 5))
+
+# Const params
+input_nodes = 4
+hidden_nodes = 2
+output_nodes = 4
+bias = 1  # on
+
+for learningrate in learning_rates:
+    for momentum in momentums:
+        # Array with number of iteration when network reach given precision
+        number_of_iters = []
+        # Array with outputs from once full cycle
+        current_tuple = []
+        print("learningrate: ", learningrate)
+        print("Momentum: ", momentum)
+        for num in range(100):
+            i = 0
+            error_test = 10
+            nn = NeutralNetwork(input_nodes, hidden_nodes,
+                                output_nodes, learningrate, bias, momentum)
+            while error_test > 5 * 10**-3:
+                for j in range(len(input_list)):
+                    nn.train(input_list[j], target_list[j])
+                f = nn.query
+                error_test = MSE(f, input_list, target_list)
+                i += 1
+
+            number_of_iters.append(i)
+        # Leaeraning rate
+        current_tuple.append(learningrate)
+        # Momentum
+        current_tuple.append(momentum)
+        # Mean of number of iter
+        current_tuple.append(np.mean(number_of_iters))
+        # Standard deviaon of  iter
+        current_tuple.append(np.std(number_of_iters, ddof=1))
+        results.append(current_tuple)
+        print(current_tuple)
 
 with open("zad2.csv", "w") as f:
     writer = csv.writer(f)
