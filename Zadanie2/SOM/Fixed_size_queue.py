62d31438050f0baa902d9ab7fbd5c2ac19b6b0e2
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-03 18:00:09 +0200

    move from kohonen network

---
+++ Zadanie2/SOM/Fixed_size_queue.py
@@ -0,0 +1,24 @@
+
+class Fixed_size_queue:
+    """
+    Data structure that contains const number of element even after append
+    """
+
+    def __init__(self, size):
+
+        # legth of queue
+        self.size = size
+        self._data = [None] * size
+
+    def append(self, obj):
+        """
+        Append element to queue and delets the lase one
+        """
+        self._data.insert(0, obj)
+        self._data.pop()
+
+    def contains(self, obj):
+        """
+        Checks if queue contains element equal to given
+        """
+        return obj in self._data
