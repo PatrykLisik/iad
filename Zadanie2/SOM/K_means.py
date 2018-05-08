281e125d3ef3558bca628056be958faabf641c92
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-09 01:00:17 +0200

    final implementation

--- Zadanie2/SOM/K_means.py
+++ Zadanie2/SOM/K_means.py
@@ -1,6 +1,5 @@
 from .functions import random_point
 from .functions import Euklides_dist as E_dist
-from .functions import GNF
 import numpy as np
 
 
@@ -8,7 +7,7 @@ class K_menas():
     """Implementation of k-means"""
 
     def __init__(self, points_number, points_to_aprox, neighborhood_radius=2,
-                 net_dist_to_lr=GNF, dist_func_points=E_dist,
+                 dist_func_points=E_dist,
                  lazy_numer=None, lr=0.2):
         """
         Args:
@@ -16,9 +15,6 @@ class K_menas():
             dinm_network: number of dimensions of network organization
             dist_func: callable object that takes two points of from
                         points_to_aprox and returns distance between them
-            net_dist_to_lr: callable object that takes poison of two neurons
-                            and returns learning_rate_multiplier. Through this
-                            argument WTA nad WTM approach can be achieved
             points_to_aprox: list of points to perform approximation on
             neighborhood_radius: one of parameter of net_dist_to_lr
             dist_func_points: function that return distnce between points
@@ -42,15 +38,15 @@ class K_menas():
         """
         ans = {}
         for _ in range(n):
-            ans[random_point()] = []
+            ans[random_point(2)] = []
         return ans
 
     def match_points_to_neurons(self):
         neurons = self.neurons.keys()
         for point in self.points_to_aprox:
             # find the closest neuron
-            tcn = min(neurons, key=lambda n: self.dist_func_points(n.point))
-            # assaing point to neuron
+            tcn = min(neurons, key=lambda n: self.dist_func_points(n, point))
+            # assign point to neuron
             self.neurons[tcn].append(point)
 
     def move_centers(self):
@@ -73,3 +69,6 @@ class K_menas():
     def iter_once(self):
         self.match_points_to_neurons()
         self.move_centers()
+
+    def getNeurons(self):
+        return self.neurons.keys()
