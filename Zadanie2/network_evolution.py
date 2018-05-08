92564dda9caa3385b185fd85ef6426b67862a565
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-08 21:12:27 +0200

    voronoi teselation

--- Zadanie2/network_evolution.py
+++ Zadanie2/network_evolution.py
@@ -5,6 +5,7 @@ from points_distributions import square_dist
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 import numpy as np
+from scipy.spatial import Voronoi, voronoi_plot_2d
 
 
 def plot(black, redPointsInTime, out, title):
@@ -16,7 +17,7 @@ def plot(black, redPointsInTime, out, title):
     plt.grid()
     # split black points into x and y
     black_x, black_y = zip(*black)
-    ax.scatter(black_x, black_y, color='black', s=10)
+    ax.scatter(black_x, black_y, color='black', s=7)
     plt.xlim([-10, 10])
     plt.ylim([-10, 10])
     plt.title(title)
@@ -24,9 +25,8 @@ def plot(black, redPointsInTime, out, title):
     i1 = iter(redPointsInTime)
     i2 = iter(redPointsInTime)
     next(i2)
-    #print("redPointsInTime", redPointsInTime)
+    # print("redPointsInTime", redPointsInTime)
     for nr, (xy_cur, xy_next) in enumerate(zip(i1, i2)):
-        label = "Iteracja nr {}".format(nr)
         xs_cur, ys_cur = zip(*xy_cur)
         xs_next, ys_next = zip(*xy_next)
         color = next(colors)
@@ -41,12 +41,32 @@ def plot(black, redPointsInTime, out, title):
     plt.savefig(out + ".png", bbox_extra_artists=(lgd,), bbox_inches='tight')
 
 
+def plotVoronoi(black, redPointsInTime, out, title):
+
+    vor = Voronoi(redPointsInTime[-1])
+    voronoi_plot_2d(vor)
+    # Set up plot
+    fig = plt.figure(figsize=(10, 10))
+    fig.add_subplot(111)
+    l, = plt.plot([], [], 'r-')
+    plt.grid()
+    # split black points into x and y
+    black_x, black_y = zip(*black)
+    plt.scatter(black_x, black_y, color='black', s=20)
+
+    plt.title(title)
+    plt.scatter(black_x, black_y, color='black', s=1)
+    plt.xlim([-10, 10])
+    plt.ylim([-10, 10])
+    plt.savefig(out + ".png")
+
+
 points = circumference_dist([0, 0], 7, 400)
 
 Neuron_number = 20
 som = {"Gas_Neuronowy": NG,
        "Siec_kohonena": KN}
-iter_number = 6
+iter_number = 8
 for decription, type in som.items():
     map = type(Neuron_number, points)
     redInTime = []
@@ -55,3 +75,4 @@ for decription, type in som.items():
         redInTime.append(neuron_pos)
         map.iter_once()
     plot(points, redInTime, decription + "_in_time", decription)
+    plotVoronoi(points, redInTime, decription + "_vor", decription)
