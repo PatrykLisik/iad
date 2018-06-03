715c64ad31c0d59a12f278227c966e270fa1cf7c
Patryk Lisik <patrykkbkak@gmail.com>
2018-06-03 17:13:24 +0200

    learnig works on lin layer

--- Zadanie3/Lin_layer.py
+++ Zadanie3/Lin_layer.py
@@ -2,12 +2,16 @@ import numpy
 
 
 class Lin_layer():
-    def __init__(self, input_number, output_number):
-        self.weigths = numpy.random.rand(output_number, input_number)
-        self.bias = numpy.random.rand()
+    def __init__(self, input_number, output_number, lr=0.1):
+        self.weigths = numpy.random.rand(output_number, input_number) * 2 - 1
+        self.bias = numpy.random.rand() * 2 - 1
+        self.lr = lr
 
     def query(self, input):
         return list((numpy.dot(self.weigths, input) + self.bias)[0])
 
-    def train(input, target):
-        pass
+    def train(self, input, layer_out, target):
+        output_errors = target - layer_out
+        self.weigths += (self.lr * numpy.dot(output_errors,
+                                             numpy.transpose(input)))
+        self.bias += self.lr * output_errors
