2609bf83e00e1d8773f127e10d9c140341397554
Patryk Lisik <patrykkbkak@gmail.com>
2018-06-20 09:11:47 +0200

    redundant param deleted

--- Zadanie3/Lin_layer.py
+++ Zadanie3/Lin_layer.py
@@ -5,7 +5,6 @@ class Lin_layer():
     def __init__(self, input_number, output_number, lr=0.1):
         self.weigths = numpy.random.rand(output_number, input_number) * 2 - 1
         self.bias = numpy.random.rand() * 2 - 1
-        self.lr = lr
 
     def query(self, input):
         return (numpy.dot(self.weigths, input) + self.bias)[0]
